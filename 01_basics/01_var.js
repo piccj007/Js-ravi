23d1f008e4354a2cc6fcb622cb554910d186c44b
piccj007 <piccj007@gmail.com>
2024-09-13 08:59:33 +0000

    data type

---
+++ 01_basics/01_var.js
@@ -0,0 +1,11 @@
+const accountId = 144544
+let accountEmail = "piccj007@gmail.com"
+var password = "123123"
+accountCity = "Talod"
+let accountState;
+// accountId = 2
+accountEmail="Piccj007@gmail.com"
+console.log(accountId)
+console.log(accountEmail)
+console.table([accountId,accountEmail,password,accountCity,accountState])
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log("ravi");
+console.log("Total :" + 5+4);
+let name = "ravi"
+let age = 18
+let IsLogin = false
+let per = 5.5
+let stand = null
+let mark = undefined
+let on = Symbol
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(IsLogin));
+console.log(typeof(per));
+console.log(typeof(stand));
+console.log(typeof(mark));
+console.log(typeof(on));
+

2d8ba0bf19c46e8d018e90a341f71b288fce2666
piccj007 <piccj007@gmail.com>
2024-09-13 09:11:15 +0000

    conversion control

---
+++ 01_basics/03_conop.js
@@ -0,0 +1,10 @@
+let score = "33"
+let score1 = 33
+let score2 = true
+console.log(typeof score);
+let valueInNumber =Number(score);
+let valueInNumber1 =Boolean(score1);
+let valueInNumber2 =String(score2);
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(typeof valueInNumber2);

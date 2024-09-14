14637f996c0a0a307e6ad93f1be3257276a6a388
piccj007 <piccj007@gmail.com>
2024-09-14 04:45:41 +0000

    data type commite

--- 01_basics/03_conop.js
+++ 01_basics/03_conop.js
@@ -1,10 +1,31 @@
-let score = "33"
-let score1 = 33
-let score2 = true
-console.log(typeof score);
-let valueInNumber =Number(score);
-let valueInNumber1 =Boolean(score1);
-let valueInNumber2 =String(score2);
-console.log(typeof valueInNumber);
-console.log(typeof valueInNumber1);
-console.log(typeof valueInNumber2);
+// let score = "33"
+// let score1 = 33
+// let score2 = true
+// console.log(typeof score);
+// let valueInNumber =Number(score);
+// let valueInNumber1 =Boolean(score1);
+// let valueInNumber2 =String(score2);
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber1);
+// console.log(typeof valueInNumber2);
+let r = 3
+let a = -r
+// console.log(a);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// // console.log(2%2);
+// console.log(1+"1");
+// console.log("1"+(1+1));
+// console.log(1+1+"1");
+// console.log(+true);
+console.log(+"");
+let num1,num2,num3
+
+
+
+
+
+
---
+++ 01_basics/04_comp.js
@@ -0,0 +1,10 @@
+// console.log(14 === 1);
+// console.log("02">1);
+// const id = Symbol('1')
+// const id1 = Symbol('1')
+// console.log(id===id1);
+const myfn = function myfnn() {
+    console.log("llll");
+    
+}
+console.log(typeof myfn);

093d7f9ba273d0d4eda081fa0978dd689974c934
piccj007 <piccj007@gmail.com>
2024-09-14 04:57:19 +0000

    memory

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,13 @@
+let s="ravi v suthar"
+let a=s;
+a = "RAvi"
+console.log(s);
+console.log(a);
+let user = {
+    userId:"a@a.com",
+    userName:"ravi"
+}
+let user1=user
+user1.userName="Moti"
+console.log(user1)
+console.log(user);

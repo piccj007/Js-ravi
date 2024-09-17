de70e8ea826b609cb85201b032ce9ed875a02048
piccj007 <piccj007@gmail.com>
2024-09-17 08:55:37 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,1 +1,26 @@
 console.log("ravi");
+const myarr = [0,1,2,3,4,5,6]
+//console.log(myarr[0]);
+const myh = ["miau", "miau miau"]
+const myh1 = ["miau1", "miau miau1"]
+const myh2 = ["miau2", "miau miau2"]
+const mya1= new Array(1,2,3,4)
+ mya1.push(1,2,3)
+// console.log(mya1);
+// mya1.pop()
+//console.log(mya1);
+//mya1.unshift(9)
+// console.log(mya1);
+mya1.shift()
+// console.log(mya1);
+// console.log(mya1.includes(9));
+// console.log(mya1.indexOf(9));
+// // const newArr = mya1.join()
+// // console.log(newArr, mya1);
+// const a = myarr.slice(1,3)
+// const b = myarr.splice(1,4)
+// console.log(myarr);
+// console.log(a);
+// console.log(b);
+
+

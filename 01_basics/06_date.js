77adf5379ccaea2c168ebc81726dbde0e7972c58
piccj007 <piccj007@gmail.com>
2024-09-17 08:28:34 +0000

    string

---
+++ 01_basics/06_date.js
@@ -0,0 +1,11 @@
+let myDate = new Date(2024,8,17)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString('MM'));
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+console.log(myDate)
---
+++ 01_basics/06_num.js
@@ -0,0 +1,17 @@
+const n = 500
+// console.log(n);
+const a = new Number(100)
+// console.log(a.toFixed(2));
+const a1 = 23.8966
+// console.log(a1.toPrecision(21));
+const a2 = 1000000
+// console.log(a2.toLocaleString('en-IN'));
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.pow(2,50))
+console.log((Math.random()*10).toFixed(1)+1)
+
+

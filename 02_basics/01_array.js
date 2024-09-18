3f94c5c9f937a8354c8ed90c6847893528bb05cd
piccj007 <piccj007@gmail.com>
2024-09-18 08:06:40 +0000

    array 2

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,17 +1,17 @@
 console.log("ravi");
-const myarr = [0,1,2,3,4,5,6]
+const myarr = [0,1,2,3,[4,5,6],7,[6,7,4,[4,5]]]
 //console.log(myarr[0]);
 const myh = ["miau", "miau miau"]
 const myh1 = ["miau1", "miau miau1"]
 const myh2 = ["miau2", "miau miau2"]
-const mya1= new Array(1,2,3,4)
- mya1.push(1,2,3)
+//const mya1= new Array(1,2,3,4)
+ //mya1.push(1,2,3)
 // console.log(mya1);
 // mya1.pop()
 //console.log(mya1);
 //mya1.unshift(9)
 // console.log(mya1);
-mya1.shift()
+//mya1.shift()
 // console.log(mya1);
 // console.log(mya1.includes(9));
 // console.log(mya1.indexOf(9));
@@ -21,6 +21,20 @@ mya1.shift()
 // const b = myarr.splice(1,4)
 // console.log(myarr);
 // console.log(a);
-// console.log(b);
+//myh1.push(myh2)
+
+//const a = myh1.concat(myh2)
+//const a = [...myh1,...myh2]
+//console.log(myarr);
+
+// const a = myarr.flat(Infinity)
+//  console.log(a);
+// console.log(Array.isArray("Ravi"));
+// console.log(Array.from("ravi"));
+// console.log(Array.from({nmae : "ravi"}));
+let score1 = 100
+let score2 = 100
+let score3 = 100
+console.log(Array.of(score1,score2, score3));
 
 
